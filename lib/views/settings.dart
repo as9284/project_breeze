06ad3899417579a669405dcfb6fdc52de11c2c62
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2025-04-10 22:37:25 +0300

    Expand settings page

--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -1,4 +1,6 @@
+import 'package:breeze/core/utils/auth_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class SettingsPage extends StatefulWidget {
   final void Function(bool) onThemeChanged;
@@ -34,6 +36,13 @@ class _SettingsPageState extends State<SettingsPage> {
       ),
       body: ListView(
         children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 10, top: 20, bottom: 10),
+            child: Text(
+              "General Settings",
+              style: TextStyle(fontWeight: FontWeight.w600, fontSize: 20),
+            ),
+          ),
           SwitchListTile(
             title: const Text("Dark Mode"),
             value: _darkMode,
@@ -42,6 +51,54 @@ class _SettingsPageState extends State<SettingsPage> {
                 _darkMode = value;
               });
               widget.onThemeChanged(value);
+            },
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text("Log Out"),
+            onTap: () async {
+              signOut(context);
+            },
+          ),
+
+          ListTile(
+            leading: const Icon(Icons.delete, color: Colors.red),
+            title: const Text(
+              "Delete Account",
+              style: TextStyle(color: Colors.red),
+            ),
+            onTap: () async {
+              final confirm = await showDialog<bool>(
+                context: context,
+                builder:
+                    (_) => AlertDialog(
+                      title: const Text("Delete Account"),
+                      content: const Text(
+                        "Are you sure you want to permanently delete your account? This action cannot be undone.",
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, false),
+                          child: const Text("Cancel"),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, true),
+                          child: const Text(
+                            "Delete",
+                            style: TextStyle(color: Colors.red),
+                          ),
+                        ),
+                      ],
+                    ),
+              );
+
+              if (confirm == true) {
+                final supabase = Supabase.instance.client;
+                final userId = Supabase.instance.client.auth.currentUser?.id;
+
+                if (userId != null) {}
+              }
             },
           ),
         ],
